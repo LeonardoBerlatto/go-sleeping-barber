a407c7a3f35cf0c1d4697af68990dfad751f0e4e
LeonardoBerlatto <leoberlatto@gmail.com>
2025-02-18 19:19:11 -0300

    Add new random duration method

--- internal/sleep/random.go
+++ internal/sleep/random.go
@@ -8,3 +8,7 @@ import (
 func RandomSleep(max time.Duration) {
 	time.Sleep(time.Duration(rand.Intn(int(max))))
 }
+
+func Random(min, max time.Duration) time.Duration {
+	return time.Duration(rand.Intn(int(max-min)) + int(min))
+}
