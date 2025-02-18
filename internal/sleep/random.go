d7190a7b016d87e5dab322d28aff0aa63eee8470
LeonardoBerlatto <leoberlatto@gmail.com>
2025-02-18 19:20:27 -0300

    Remove unused

--- internal/sleep/random.go
+++ internal/sleep/random.go
@@ -5,10 +5,6 @@ import (
 	"time"
 )
 
-func RandomSleep(max time.Duration) {
-	time.Sleep(time.Duration(rand.Intn(int(max))))
-}
-
 func Random(min, max time.Duration) time.Duration {
 	return time.Duration(rand.Intn(int(max-min)) + int(min))
 }
